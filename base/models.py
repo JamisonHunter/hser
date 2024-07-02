de0ddd10ff2fa912a26b0556fa3709bf6d59bda9
Jamison Hunter <hunterjamison6@gmail.com>
2024-07-01 21:04:54 -0400

    Added Image functionality to models.py

---
+++ base/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=300, null=True, blank=True)
+    serial = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
