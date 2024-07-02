bcdd82c2f2ee7beff3368d0f9d09c3474962cd5c
Jamison Hunter <hunterjamison6@gmail.com>
2024-07-02 02:10:01 -0400

    Created new item submit form and route

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home, ItemDetail
+from .views import Home, ItemDetail, NewItem
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("item/<int:pk>/", ItemDetail.as_view(), name="item"),
+    path("new-item", NewItem.as_view(), name="new-item"),
 ]   
--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Item
 
 # Create your views here.
@@ -10,3 +12,8 @@ class Home(ListView):
 
 class ItemDetail(DetailView):
     model = Item
+
+class NewItem(CreateView):
+    model = Item
+    fields = ["name", "serial", "image"]
+    success_url = reverse_lazy("home")
