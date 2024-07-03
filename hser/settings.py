35a646b16909b45dfe9330527151f0bf55ae26cc
Jamison Hunter <hunterjamison6@gmail.com>
2024-07-02 22:34:30 -0400

    Began building CSS and rearranging HTML

--- hser/settings.py
+++ hser/settings.py
@@ -116,8 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -132,4 +130,12 @@ USE_TZ = True
 TIME_ZONE = 'UTC'
 
 LOGIN_URL = "login"
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# For collecting static files in production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
